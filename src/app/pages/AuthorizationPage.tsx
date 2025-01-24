e8ab41c8ef7ce0e22d4241247dd75cf0a2a98c74
anton <antonkuzmn1@gmail.com>
2025-01-24 23:51:12 +0700

    added registration

--- src/app/pages/AuthorizationPage.tsx
+++ src/app/pages/AuthorizationPage.tsx
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import {AppDispatch} from "../../utils/store";
 import {useDispatch} from "react-redux";
-import {setAppError, setAppLoading} from "../../slices/appSlice";
+import {setAppError, setAppLoading, setAppMessage} from "../../slices/appSlice";
 import axios from "axios";
 import Cookies from "js-cookie";
 import {setAccountAuthorized} from "../../slices/accountSlice.ts";
@@ -9,8 +9,11 @@ import {setAccountAuthorized} from "../../slices/accountSlice.ts";
 const Login = () => {
     const dispatch: AppDispatch = useDispatch();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const [register, setRegister] = useState<boolean>(false);
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
@@ -19,7 +22,6 @@ const Login = () => {
             return;
         }
 
-        console.log(email, password);
         dispatch(setAppLoading(true));
         try {
             axios.post(
@@ -52,6 +54,103 @@ const Login = () => {
             dispatch(setAppLoading(false));
         }
     };
+
+    const handleSubmitRegister = async (e: any) => {
+        e.preventDefault();
+        if (!username || !email || !password) {
+            dispatch(setAppError('fields "username", "email" and "password" is required'))
+            return;
+        }
+
+        dispatch(setAppLoading(true));
+        try {
+            axios.post(import.meta.env.VITE_BASE_URL + '/users', {
+                username,
+                email,
+                password,
+            }).then((response) => {
+                console.log(response)
+                dispatch(setAppMessage('Successfully registered!'));
+            }).catch((error) => {
+                dispatch(setAppError(error.message));
+            }).finally(() => {
+                dispatch(setAppLoading(false));
+            });
+        } catch (error: any) {
+            console.error('error:', error);
+            dispatch(setAppError(error.response.data))
+            Cookies.remove('token');
+            delete axios.defaults.headers.common['Authorization'];
+            dispatch(setAccountAuthorized(false));
+            dispatch(setAppLoading(false));
+        }
+    };
+
+
+    if (register) {
+        return (
+            <div className="fixed z-50 inset-0 flex items-center justify-center h-screen bg-white">
+                <div className="p-6 w-full max-w-sm">
+                    <h2 className="text-2xl font-bold text-center mb-6">Registration</h2>
+                    <form onSubmit={handleSubmitRegister} className="space-y-4">
+                        <div>
+                            <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                                Username
+                            </label>
+                            <input
+                                type="username"
+                                id="username"
+                                className="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                placeholder="Enter username"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                className="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="example@mail.com"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                className="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="Enter password"
+                                required
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md transition duration-300"
+                        >
+                            Submit
+                        </button>
+                    </form>
+                    <p className="text-center text-sm text-gray-600 mt-4">
+                        <button className="text-blue-500 hover:underline" onClick={() => setRegister(false)}>
+                            Sign in
+                        </button>
+                    </p>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className="fixed z-50 inset-0 flex items-center justify-center h-screen bg-white">
@@ -94,7 +193,7 @@ const Login = () => {
                     </button>
                 </form>
                 <p className="text-center text-sm text-gray-600 mt-4">
-                    <button className="text-blue-500 hover:underline">
+                    <button className="text-blue-500 hover:underline" onClick={() => setRegister(true)}>
                         Create new account
                     </button>
                 </p>
